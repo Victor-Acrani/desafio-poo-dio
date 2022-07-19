1456ec9fd59cf0668315663af2956df733ebf265
Victor-Acrani <victoracrani@gmail.com>
2022-07-18 23:42:06 -0300

    Commit aula5 - interligando as classes

--- src/br/com/acrani/Main.java
+++ src/br/com/acrani/Main.java
@@ -1,6 +1,8 @@
 package br.com.acrani;
 
+import br.com.acrani.models.Bootcamp;
 import br.com.acrani.models.Course;
+import br.com.acrani.models.Developer;
 import br.com.acrani.models.Mentoring;
 
 import java.time.LocalDate;
@@ -8,13 +10,48 @@ import java.time.LocalDate;
 public class Main {
 
     public static void main(String[] args) {
-        Course course = new Course("Java 1", "Basic java course", 30);
+        Course course1 = new Course("Java 1", "Basic java course", 30);
+        Course course2 = new Course("Java 2", "Senior java course", 55);
         Mentoring mentoring = new Mentoring("Java Bootcamp",
                 "Advanced java bootcamp", LocalDate.of(2022,10,20));
 
-        System.out.println(course);
-        System.out.println("Course name: " + course.getTitle() + " ,total XP: " + course.calculateXP());
+        System.out.println(course1);
+        System.out.println("Course name: " + course1.getTitle() + " ,total XP: " + course1.calculateXP());
         System.out.println(mentoring);
         System.out.println("Mentoring name: " + mentoring.getTitle() + " ,total XP: " + mentoring.calculateXP());
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setName("Bootcamp Java Developer");
+        bootcamp.getContents().add(course1);
+        bootcamp.getContents().add(course2);
+        bootcamp.getContents().add(mentoring);
+
+        System.out.println("-------------");
+
+        Developer devCamila = new Developer();
+        devCamila.setName("Camila");
+        devCamila.subscribeBootcamp(bootcamp);
+        System.out.println("Camila's subscribed courses: " + devCamila.getSubscribedContents());
+        devCamila.progress();
+        System.out.println("-");
+        System.out.println("Camila's subscribed courses: " + devCamila.getSubscribedContents());
+        System.out.println("Camila's finished courses: " + devCamila.getFinishedContents());
+        System.out.println("XP: " + devCamila.calculateXP());
+
+        System.out.println("-------------");
+
+        Developer devJoao = new Developer();
+        devJoao.setName("Joao");
+        devJoao.subscribeBootcamp(bootcamp);
+        System.out.println("Joao's subscribed courses: " + devJoao.getSubscribedContents());
+        devJoao.progress();
+        System.out.println("-");
+        System.out.println("Joao's subscribed courses: " + devJoao.getSubscribedContents());
+        devJoao.progress();
+        System.out.println("-");
+        System.out.println("Joao's subscribed courses: " + devJoao.getSubscribedContents());
+        System.out.println("Joao's finished courses: " + devJoao.getFinishedContents());
+        System.out.println("XP: " + devJoao.calculateXP());
+
     }
 }
